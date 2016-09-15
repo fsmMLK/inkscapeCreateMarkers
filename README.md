b260c93dbf2e2234aed9b943416323c04b4a300e
fsmMLK <fsmMLK@users.noreply.github.com>
2016-09-14 21:45:41 -0300

    first version of the documentation

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # createMarkers
 Inkscape extension to assist creating new markers with custom shape and colors.
+
+<img src="docs/images/Examples.png" alt="Drawing" style="width: 300px;"/>
+
+# Usage
+
+This extension is presented in two tabs. The **Config** tab allows you set most of the options of your marker, while the **Color** tab allows you to select both stroke and filling colors.
+
+<img src="docs/images/Screen_01.png" alt="Config" style="width: 300px;"/>
+<img src="docs/images/Screen_02.png" alt="Color" style="width: 300px;"/>
+
+### The Config tab
+
+This tab is divided in three sections, on the first section **Predefined types**, you can create three predefined markers:
+
+ - **Dot**: similar to some of the standard markers present in Inkscape, but you can choose the filling and stroke colors  independently
+ - **Ellipsis**: this marker differs from Inkscape's standard marker (InfiniteLine) since the dots in this marker will follow the line width of your line (if scale factor is set to 1.0)
+ - **Arrow1**: similar to Inkscape's standard Arrow1 marker, but you can choose the filling and stroke colors independently
+
+<img src="docs/images/predefined_types.png" alt="Drawing" style="width: 300px;"/>
+
+To create them, you just have to check the respective checkbox (you can check more than one). For each predefined marker you can give one nameID. Please see below how to configure its behavior when the extensions is faced with conflicting marker nameIDs.
+
+In the second section **Custom type** you  can define your own custom markers. To do so, you must provide a valid SVG path's 'd' attribute. Please check any SVG reference on syntax, e.g., <https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Paths>. **Attention** This extension will not check the syntax of your definition. Luckily, providing an invalid attribute does not cause serious crashes, therefore you can activate the 'Live preview' checkbox to help you.
+
+In the third section **General config settings** you can adjust the scale of your marker and the behavior in case of conflicting nameIDs (names already taken). You have three options:
+
+  - **Do not modify the marker:**. If the nameID is already taken by another marker, the extension quits without modifying the marker
+  - **Overwrites definition:**. If the nameID is already taken by another marker, the extension overwrites the marker. **Attention:** This will modify all instances of that marker in your document!
+  - **Add suffix number to nameID:** If the nameID is already taken by another marker, Adds a suffix _nXXXXX, where XXXXX is a number that guarantees that nameID+_nXXXXX is unique.
+
+
+### The Color tab
+
+This tab is divided in two sections, the first refer to the filling color while the second to the stroke color.
+
+In both cases, you can select a few predefined colors in the color drop down menu. Or select  **none** to set no color (transparent).
+
+You can also select **use color picker** to choose the color from the color picker widget. **Attention:** the color selected in the color picker widget will be used **ONLY** if you select **use color picker** in the drop down menu. 
+
+<img src="docs/images/Default_colors.png" alt="Color" style="width: 300px;"/>
+
+### Observations
+
+1- If you create more than one marker at once, then they will all share the same filling and stroke colors
+
+2- One small line segment is created when you run the extension so that you can visualize the marker. This is specially useful when you check the 'Live preview' box.
+
+3- The system of coordinates of the marker depends on the point under consideration. The following figure presents the coordinate system for all start, mid and end points. **Remember that +y points down in Inkscape!**
+
+<img src="docs/images/marker_Orientation.png" alt="Drawing" style="width: 600px;"/>
+
+# Behind the scene.
+
+Behind the scenes, this extension is using the extension inkscapeMadeEasy  <https://fsmmlk.github.io/inkscapeMadeEasy/>, mainly two classes defined in inkscapeMadeEasy_Draw to manage colors and create markers.
+
+# Examples
+
+<img src="docs/images/Examples.png" alt="Drawing" style="width: 300px;"/>
+
+
+Blue cross: `M -3,3 L 3,-3 M 3,3 L -3,-3`
+
+Smile boy: `M -5,0  a 5 5 0 1 1 10 0 a 5 5 0 1 1 -10 0 M -2,-3 v 2  M 2,-3 v 2  M -3,0  a 2 2 0 1 0 6 0 z`
+
+Pac Boy: `M 4,3  a 5,5 0 0 1 -4 2 a 5,5 0 0 1 0,-10 a 5 ,5 0 0 1 4,2 L 0,0 z`
+
+Ghosty (small): `M -5,0  a 5,5 0 0 1 10 0 l 0 6 l -2.5,-2.5 -2.5,2.5 -2.5,-2.5 -2.5,2.5 z M -2,1 l -0 -3 M 2,1 l -0 -3`
+
+Ghosty (large): The same, but with different colors and scale factor
+
+# Installation and requirements
+
+This extension requires another extension to run, inkscapeMadeEasy <https://github.com/fsmMLK/inkscapeMadeEasy>, which contains several backstage methods and classes.
+
+In order to use createMarkers extension, you must also download inkscapeMadeEasy files and put them both inside Inkscape's extension directory. Please refer to inkscapeMadeEasy installation instructions. In the end you must have the following files and directories in your Inkscape extension directory.
+
+```
+inkscape/extensions/
+            |-- inkscapeMadeEasy_Base.py
+            |-- inkscapeMadeEasy_Draw.py
+            |-- inkscapeMadeEasy_Plot.py
+            |-- textextLib
+            |   |-- __init__.py
+            |   |-- basicLatexPackages.tex
+            |   |-- textext.inx
+            |   |-- textext.py
+            |
+            |-- createMarkers.py
+            `-- createMarkers.inx
+```
+
--- createMarkers.py
+++ createMarkers.py
@@ -1,85 +1,102 @@
 #!/usr/bin/python
 
+# --------------------------------------------------------------------------------------
+#
+#    createMarkers: - Inkscape extension to assist creating new markers with
+#                     custom shape and colors. 
+#
+#    Copyright (C) 2016 by Fernando Moura
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# --------------------------------------------------------------------------------------
 
+import os
 import inkex
 import inkscapeMadeEasy_Base as inkBase
 import inkscapeMadeEasy_Draw as inkDraw
 import math
 
-#some symbol definition
-
-Ohm=u'\u2126'
-
 #---------------------------------------------
+
 class CreateMarkers(inkBase.inkscapeMadeEasy):
-  def __init__(self):
-    inkex.Effect.__init__(self)
-    
-    self.OptionParser.add_option("--tab",action="store", type="string",dest="tab", default="object") 
-    
-    self.OptionParser.add_option("--makeDot",action="store", type="inkbool",dest="makeDot", default=False)
-    self.OptionParser.add_option("--makeEllipsis",action="store", type="inkbool",dest="makeEllipsis", default=False)
-    self.OptionParser.add_option("--makeArrow1",action="store", type="inkbool",dest="makeArrow1", default=False)
-    self.OptionParser.add_option("--makeCustom",action="store", type="inkbool",dest="makeCustom", default=False)
-    
-    self.OptionParser.add_option("--makeDotName", action="store", type="string", dest="makeDotName", default='myDot') 
-    self.OptionParser.add_option("--makeEllipsisName", action="store", type="string", dest="makeEllipsisName", default='myEllip') 
-    self.OptionParser.add_option("--makeArrow1Name", action="store", type="string", dest="makeArrow1Name", default='myArrow') 
-    self.OptionParser.add_option("--makeCustomName", action="store", type="string", dest="makeCustomName", default='myCustom') 
-    
-    
-    self.OptionParser.add_option("--makeCustomPathDef", action="store", type="string", dest="makeCustomPathDef", default='M -5,5 L 5,-5 M 5,5 L -5,-5') 
 
+    def __init__(self):
+        inkex.Effect.__init__(self)
 
-    self.OptionParser.add_option("--scale", action="store", type="float", dest="scale", default=1)
-    self.OptionParser.add_option("--renameMode", action="store", type="string", dest="renameMode", default='2')
-    
-    self.OptionParser.add_option("--markColor", action="store", type="string", dest="markColorOption", default='black')
-    self.OptionParser.add_option("--colorPickerMark", action="store", type="string", dest="colorPickerMark", default='0') 
-        
-    self.OptionParser.add_option("--lineColor", action="store", type="string", dest="lineColorOption", default='black') 
-    self.OptionParser.add_option("--colorPickerLine", action="store", type="string", dest="colorPickerLine", default='0') 
-  
-  def effect(self):
-    
-    so = self.options
-    
-    # sets the position to the viewport center, round to next 10.
-    position=[self.view_center[0],self.view_center[1]]
-    position[0]=int(math.ceil(position[0] / 10.0)) * 10
-    position[1]=int(math.ceil(position[1] / 10.0)) * 10
-    
-    #root_layer = self.current_layer
-    root_layer = self.document.getroot()
-    
-    # sets colors. See inkDraw.color class for instructions.
-    [markerColor,alpha]=inkDraw.color.parseColorPicker(so.markColorOption,so.colorPickerMark)
-    [lineColor,alpha]=inkDraw.color.parseColorPicker(so.lineColorOption,so.colorPickerLine)
-        
-   
-    if so.makeDot:
-      nameMarkerDot=inkDraw.marker.createDotMarker(self,so.makeDotName+'_'+str(so.scale),RenameMode=int(so.renameMode),scale=0.4*so.scale,strokeColor=lineColor,fillColor=markerColor)
-      lineStylePlot = inkDraw.lineStyle.set(lineWidth=1,lineColor=lineColor,markerStart=nameMarkerDot,markerEnd=nameMarkerDot)
-      inkDraw.line.relCoords(root_layer, [[10,0],[10,-10]],[position[0],position[1]],lineStyle=lineStylePlot)
-      
-    if so.makeEllipsis:
-      startMarker,endMarker = inkDraw.marker.createInfLineMarker(self,so.makeEllipsisName+'_'+str(so.scale),RenameMode=int(so.renameMode),scale=so.scale,fillColor=markerColor)
-      lineStylePlot = inkDraw.lineStyle.set(lineWidth=1,lineColor=lineColor,markerStart=startMarker,markerEnd=endMarker)
-      inkDraw.line.relCoords(root_layer, [[10,0],[10,-10]],[position[0],position[1]+5],lineStyle=lineStylePlot)
-      
-    if so.makeArrow1:
-      startMarker,endMarker=inkDraw.marker.createArrow1Marker(self,so.makeArrow1Name+'_'+str(so.scale),RenameMode=int(so.renameMode),scale=0.4*so.scale,strokeColor=lineColor,fillColor=markerColor)
-      lineStylePlot = inkDraw.lineStyle.set(lineWidth=1,lineColor=lineColor,markerStart=startMarker,markerEnd=endMarker)
-      inkDraw.line.relCoords(root_layer, [[10,0],[10,-10]],[position[0],position[1]+10],lineStyle=lineStylePlot)
-      
-    if so.makeCustom:
-      markerPath=so.makeCustomPathDef
-      markerTransform = 'scale(' + str(so.scale) + ')'
-      Marker=inkDraw.marker.createMarker(self,so.makeCustomName+'_'+str(so.scale),markerPath=markerPath,RenameMode=int(so.renameMode),strokeColor=lineColor,fillColor=markerColor,markerTransform=markerTransform)
-      lineStylePlot = inkDraw.lineStyle.set(lineWidth=1,lineColor=lineColor,markerStart=Marker,markerEnd=Marker)
-      inkDraw.line.relCoords(root_layer, [[10,0],[10,-10]],[position[0],position[1]+20],lineStyle=lineStylePlot)
-      
+        self.OptionParser.add_option("--tab", action="store", type="string", dest="tab", default="object")
+
+        self.OptionParser.add_option("--makeDot", action="store", type="inkbool", dest="makeDot", default=False)
+        self.OptionParser.add_option("--makeEllipsis", action="store", type="inkbool", dest="makeEllipsis", default=False)
+        self.OptionParser.add_option("--makeArrow1", action="store", type="inkbool", dest="makeArrow1", default=False)
+        self.OptionParser.add_option("--makeCustom", action="store", type="inkbool", dest="makeCustom", default=False)
+
+        self.OptionParser.add_option("--makeDotName", action="store", type="string", dest="makeDotName", default='myDot')
+        self.OptionParser.add_option("--makeEllipsisName", action="store", type="string", dest="makeEllipsisName", default='myEllip')
+        self.OptionParser.add_option("--makeArrow1Name", action="store", type="string", dest="makeArrow1Name", default='myArrow')
+        self.OptionParser.add_option("--makeCustomName", action="store", type="string", dest="makeCustomName", default='myCustom')
+
+        self.OptionParser.add_option("--makeCustomPathDef", action="store", type="string", dest="makeCustomPathDef", default='M -5,5 L 5,-5 M 5,5 L -5,-5')
+        self.OptionParser.add_option("--makeCustomLineWidth", action="store", type="float", dest="makeCustomLineWidth", default=1.0)
+
+        self.OptionParser.add_option("--scale", action="store", type="float", dest="scale", default=1)
+        self.OptionParser.add_option("--renameMode", action="store", type="string", dest="renameMode", default='2')
+
+        self.OptionParser.add_option("--markColor", action="store", type="string", dest="markColorOption", default='black')
+        self.OptionParser.add_option("--colorPickerMark", action="store", type="string", dest="colorPickerMark", default='0')
+
+        self.OptionParser.add_option("--lineColor", action="store", type="string", dest="lineColorOption", default='black')
+        self.OptionParser.add_option("--colorPickerLine", action="store", type="string", dest="colorPickerLine", default='0')
+
+    def effect(self):
+
+        so = self.options
+
+        # sets the position to the viewport center, round to next 10.
+        position = [self.view_center[0], self.view_center[1]]
+        position[0] = int(math.ceil(position[0] / 10.0)) * 10
+        position[1] = int(math.ceil(position[1] / 10.0)) * 10
+
+        #root_layer = self.current_layer
+        root_layer = self.document.getroot()
+
+        # sets colors. See inkDraw.color class for instructions.
+        [markerColor, alpha] = inkDraw.color.parseColorPicker(so.markColorOption, so.colorPickerMark)
+        [lineColor, alpha] = inkDraw.color.parseColorPicker(so.lineColorOption, so.colorPickerLine)
+
+        if so.makeDot:
+            nameMarkerDot = inkDraw.marker.createDotMarker(self, so.makeDotName + '_' + str(so.scale), RenameMode=int(so.renameMode), scale=0.4 * so.scale, strokeColor=lineColor, fillColor=markerColor)
+            lineStylePlot = inkDraw.lineStyle.set(lineWidth=1, lineColor=lineColor, markerStart=nameMarkerDot, markerEnd=nameMarkerDot)
+            inkDraw.line.relCoords(root_layer, [[10, 0], [10, -10]], [position[0], position[1]], lineStyle=lineStylePlot)
+
+        if so.makeEllipsis:
+            startMarker, endMarker = inkDraw.marker.createInfLineMarker(self, so.makeEllipsisName + '_' + str(so.scale), RenameMode=int(so.renameMode), scale=so.scale, fillColor=markerColor)
+            lineStylePlot = inkDraw.lineStyle.set(lineWidth=1, lineColor=lineColor, markerStart=startMarker, markerEnd=endMarker)
+            inkDraw.line.relCoords(root_layer, [[10, 0], [10, -10]], [position[0], position[1] + 5], lineStyle=lineStylePlot)
+
+        if so.makeArrow1:
+            startMarker, endMarker = inkDraw.marker.createArrow1Marker(self, so.makeArrow1Name + '_' + str(so.scale), RenameMode=int(so.renameMode), scale=0.4 * so.scale, strokeColor=lineColor, fillColor=markerColor)
+            lineStylePlot = inkDraw.lineStyle.set(lineWidth=1, lineColor=lineColor, markerStart=startMarker, markerEnd=endMarker)
+            inkDraw.line.relCoords(root_layer, [[10, 0], [10, -10]], [position[0], position[1] + 10], lineStyle=lineStylePlot)
+
+        if so.makeCustom:
+            markerPath = so.makeCustomPathDef
+            markerTransform = 'scale(' + str(so.scale) + ')'
+            Marker = inkDraw.marker.createMarker(self, so.makeCustomName + '_' + str(so.scale), markerPath=markerPath, RenameMode=int(so.renameMode), strokeColor=lineColor, fillColor=markerColor, lineWidth=so.makeCustomLineWidth,markerTransform=markerTransform)
+            lineStylePlot = inkDraw.lineStyle.set(lineWidth=1, lineColor=lineColor, markerStart=Marker, markerEnd=Marker)
+            inkDraw.line.relCoords(root_layer, [[10, 0], [10, -10]], [position[0], position[1] + 20], lineStyle=lineStylePlot)
+
 if __name__ == '__main__':
-  markers = CreateMarkers()
-  markers.affect()
-    
+    markers = CreateMarkers()
+    markers.affect()
